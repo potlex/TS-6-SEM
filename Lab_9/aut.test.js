cef184ee235f3c5c0d855b20acc564b66fbeb74a
Aleksey Potapovich <potapovichalekseystudy@gmail.com>
2024-04-09 09:20:13 +0300

    new version

--- Lab_9/aut.test.js
+++ Lab_9/aut.test.js
@@ -1,38 +1,19 @@
-const { Builder, By, until } = require('selenium-webdriver');
-const chrome = require('selenium-webdriver/chrome');
-const { Options } = chrome;
-
-describe('Turtle routes', () => {
-    let driver;
-
-    beforeAll(async () => {
-        const options = new Options();
-        options.addArguments("--headless");
-        driver = await new Builder().forBrowser('chrome').setChromeOptions(options).build();
-    });
-
-    afterAll(async () => {
-        await driver.quit();
-    });
-
-    it('should return 400 if id is not a number', async () => {
-        await driver.get('http://localhost:3000/api/turtles/abc');
-        const responseBody = await driver.findElement(By.css('body')).getText();
-        const error = JSON.parse(responseBody);
-        expect(error.error).toBe('Invalid ID');
-    });
-
-    it('should return 404 if turtle with given id does not exist', async () => {
-        await driver.get('http://localhost:3000/api/turtles/1');
-        const responseBody = await driver.findElement(By.css('body')).getText();
-        const turtle = JSON.parse(responseBody);
-        expect(turtle.error).toBe(undefined);
-    });
-    
-    it('should return turtle if id is valid', async () => {
-        await driver.get('http://localhost:3000/api/turtles/1');
-        const responseBody = await driver.findElement(By.css('body')).getText();
-        const turtle = JSON.parse(responseBody);
-        expect(turtle).toEqual({ id: 1, name: 'Leonardo', color: 'Blue', weaponId: 1, favoritePizzaId: 2, secondFavoritePizzaId: 5, image: null });
-    });
+test('проверка поиска товара на 5element.by', async () => {
+    const { Builder, By, Key, until } = require('selenium-webdriver');
+  
+    const driver = await new Builder().forBrowser('chrome').build();
+  
+    try {
+      await driver.get('https://5element.by/');
+      const searchField = await driver.findElement(By.css('input[placeholder="Поиск товара"]'));
+      await searchField.click();
+      await searchField.sendKeys('IPhone 15', Key.RETURN);
+      await driver.wait(until.elementLocated(By.className('product-item')), 5000);
+      const searchResults = await driver.findElements(By.className('product-item'));
+      console.log('Количество результатов поиска:', searchResults.length);
+    } catch (error) {
+      console.error('Произошла ошибка:', error);
+    } finally {
+      await driver.quit();
+    }
 });
